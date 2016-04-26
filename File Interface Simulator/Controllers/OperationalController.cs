4eee6398a9013d76bcf9895b9e9f7351253c3c7b
kmetde <kmetde@LPT00494>
2016-04-26 15:05:23 +0200

    Controllers toegevoegd

---
+++ File Interface Simulator/Controllers/OperationalController.cs
@@ -0,0 +1,17 @@
+﻿'using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace File_Interface_Simulator.Controllers
+{
+    public class OperationalController : Controller
+    {
+        // GET: Operational
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
