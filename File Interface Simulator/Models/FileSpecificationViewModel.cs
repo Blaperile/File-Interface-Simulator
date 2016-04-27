0cb02859333292fa543daab8b71b40799869a7e5
debral <debral@LPT00587>
2016-04-27 10:34:52 +0200

    FileSpecificationViewModel aanmaken

---
+++ File Interface Simulator/Models/FileSpecificationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace File_Interface_Simulator.Models
+{
+    public class FileSpecificationViewModel
+    {
+        public string Name { get; set; }
+        public bool IsInput { get; set; }
+        public string Path { get; set; }
+        public string Version { get; set; }
+        public string FieldSpecificationVersion { get; set; }
+        public string InDirectoryPath { get; set; }
+        public string ArchiveDirectoryPath { get; set; }
+        public string OutDirectoryPath { get; set; }
+    }
+}
