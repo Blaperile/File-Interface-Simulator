50687786e18487bf21f6a35c33b72f8087273fa5
debral <debral@LPT00587>
2016-05-10 12:58:02 +0200

    Aanmaken van workflow template

--- File Interface Simulator/Models/WorkflowTemplateDetailViewModel.cs
+++ File Interface Simulator/Models/WorkflowTemplateDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,8 @@ namespace File_Interface_Simulator.Models
         public string IsActive { get; set; }
         public IEnumerable<WorkflowTemplateStepDetailViewModel> CurrentWorkflowTemplateSteps { get; set; }
         public int NewSequenceNumber { get; set; }
+
+        [Required]
         public string NewStep { get; set; }
         public IEnumerable<WorkflowTemplatePossibleFileSpecificationDetailViewModel> PossibleFileSpecifications { get; set; }
     }
